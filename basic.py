8ccc917da7fa9268ee3653ed1b68ca14a92fe87b
Ritista Shrestha <ritista99@gmail.com>
2024-07-15 21:47:26 +0545

    first commit

---
+++ basic.py
@@ -0,0 +1,20 @@
+d = int(input("enter 1st number"))
+e = int(input("enter 2nd number"))
+print(type(d))
+print(d + e)
+
+print(d - e)
+
+print(d * e)
+
+print(d ** e)
+
+if (e==2):
+    print("the number is similar to 2")
+else:
+    print("value doesnot match")
+
+if(d % 2 == 0):
+    print("the number is divisible")
+else:
+    print("doesnot match")
